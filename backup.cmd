6457a54efb1870142710c2c37b2a45d899c473e9
MarkusHeinisch <markusheinisch83@googlemail.com>
2023-01-01 19:15:37 +0100

    added destination directory check and creation

--- backup.cmd
+++ backup.cmd
@@ -5,6 +5,24 @@ SET htdocsDir=C:\dev\wamp\www
 SET dateFormat=%date:~-4%-%date:~-7,2%-%date:~-10,2%
 SET destDir=D:\%dateFormat%
 
+IF EXIST %destDir% (
+	ECHO destination directory already exists!
+	ECHO 	%destDir%
+	ECHO.
+	PAUSE
+	exit
+)
+
+ECHO create destination directory...
+IF NOT EXIST %destDir% mkdir %destDir%
+IF NOT EXIST %destDir% (
+	ECHO ERROR: destination directory
+	goto :ende
+)
+ECHO DONE
+
+ECHO ----------------------------
+
 ECHO copy htdocs...
 %zipApp% %destDir%\%dateFormat%-htdocs.zip %htdocsDir% 2>nul
 if %ERRORLEVEL% NEQ 0 (
