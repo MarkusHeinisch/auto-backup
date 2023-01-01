80d0a7ff64d08da48f7ce4661f5ffbcd94c7ba90
MarkusHeinisch <markusheinisch83@googlemail.com>
2023-01-01 19:11:11 +0100

    added date format constant

--- backup.cmd
+++ backup.cmd
@@ -1,11 +1,12 @@
 @ECHO off
 
 SET zipApp=%cd%"\7zip\7z.exe" a -bso0 -bsp1
-SET htdocsDIR=C:\dev\wamp\www
-SET destDir=D:\%date:~-4%-%date:~-7,2%-%date:~-10,2%
+SET htdocsDir=C:\dev\wamp\www
+SET dateFormat=%date:~-4%-%date:~-7,2%-%date:~-10,2%
+SET destDir=D:\%dateFormat%
 
 ECHO copy htdocs...
-%zipApp% %destDir%\%date:~-4%-%date:~-7,2%-%date:~-10,2%-htdocs.zip %htdocsDIR% 2>nul
+%zipApp% %destDir%\%dateFormat%-htdocs.zip %htdocsDir% 2>nul
 if %ERRORLEVEL% NEQ 0 (
 	ECHO ERROR: zip htdocs
 	goto :ende
